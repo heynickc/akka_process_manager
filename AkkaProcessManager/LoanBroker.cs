d9f90e1ce7a8f1d350649af8dc6079462187f53a
heynickc <nick.chamberlain.jr@gmail.com>
2016-04-15 17:00:39 -0400

    loanBroker

--- AkkaProcessManager/LoanBroker.cs
+++ AkkaProcessManager/LoanBroker.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using Akka.Actor;
+using Akka.Event;
 
 namespace AkkaProcessManager {
 
@@ -51,8 +52,53 @@ namespace AkkaProcessManager {
         }
     }
 
-    public class LoanBroker : ReceiveActor {
+    public class LoanBroker : ProcessManager {
         private readonly IActorRef _creditBureau;
         private readonly List<IActorRef> _banks;
+        private readonly ILoggingAdapter _logger = Context.GetLogger();
+        public LoanBroker() {
+
+        }
+
+        private void BankLoanRateQuotedHandler(BankLoanRateQuoted message) {
+            _logger.Info("LoanBroker recieved BankLoanRateQuoted message for bankId: {0}", message.BankId);
+            ProcessOf(message.LoanQuoteReferenceId).Tell(
+                new RecordLoanRateQuote(message.BankId,
+                    message.BankLoanRateQuoteId,
+                    message.InterestRate));
+        }
+
+        private void CreditCheckedHandler(CreditChecked message) {
+            _logger.Info("LoanBroker recieved CreditChecked message for creditProcessingReferenceId: {0}", message.CreditProcessingReferenceId);
+            ProcessOf(message.CreditProcessingReferenceId).Tell(
+                new EstablishCreditScoreForLoanRateQuote(message.CreditProcessingReferenceId,
+                message.TaxId,
+                message.Score));
+        }
+
+        private void CreditScoreForLoanRateQuoteDeniedHandler(CreditScoreForLoanRateQuoteDenied message) {
+            _logger.Info("LoanBroker recieved CreditScoreForLoanRateQuoteDenied message for loanRateQuoteId: {0}", message.LoanRateQuoteId);
+            ProcessOf(message.LoanRateQuoteId).Tell(
+                new TerminateLoanRateQuote());
+            var denied = new BestLoanRateDenied(
+                message.LoanRateQuoteId,
+                message.TaxId,
+                message.Amount,
+                message.TermInMonths,
+                message.Score);
+            _logger.Info("Would be sent to original requester loanRateQuoteId: {0}", message.LoanRateQuoteId);
+        }
+
+        private void CreditScoreForLoanRateQuoteEstablishedHandler(CreditScoreForLoanRateQuoteEstablished message) {
+            _logger.Info("LoanBroker recieved CreditScoreForLoanRateQuoteEstablished message for loanRateQuoteId: {0}", message.LoanRateQuoteId);
+            foreach (var bank in _banks) {
+                bank.Tell(
+                    new QuoteLoanRate(message.LoanRateQuoteId,
+                    message.TaxId,
+                    message.Score,
+                    message.Amount,
+                    message.TermInMonths));
+            }
+        }
     }
 }
